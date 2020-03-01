a08636432705c6b6f98fdb4e76c3c426870aa91d
Molezar <61122341+Molezar@users.noreply.github.com>
2020-03-01 02:27:46 +0200

    F

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import List from './components/List';
+
+
+function App() {
+ return (
+  <div className="todo">
+    <div className="todo__sidebar">
+     <List />
+    </div>    
+      <div className="todo__tasks">      
+      </div>
+  </div>
+  );
+}
+export default App;
